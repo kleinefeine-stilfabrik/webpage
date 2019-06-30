97d9ba1e90b93d50ee563ee82da8070f42d0b299
BuildTools <unnamedDE.mail@googlemail.com>
2019-06-30 15:18:50 +0200

    Kunst & Kontakt überarbeitung

---
+++ js/geschenke.js
@@ -0,0 +1,14 @@
+fetch('../src/special-action.json')
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(myJson) {
+    // console.log(JSON.stringify(myJson));
+
+    var actionMain = document.querySelector('#special-action');
+
+    actionMain.querySelector('p:first-child').innerHTML = myJson.name;
+    actionMain.querySelector('h3').innerHTML = myJson.title;
+    actionMain.querySelector('p:last-child').innerHTML = myJson.text;
+    return;
+  });
